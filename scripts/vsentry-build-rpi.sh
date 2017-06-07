a2da5463c043e5c4bd4283872acda1b978d99c23
arturb-dev <beartur89@gmail.com>
2017-06-07 13:56:18 +0300

    [PATCH] added cross compile option for ARM

--- scripts/vsentry-build-rpi.sh
+++ scripts/vsentry-build-rpi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install libncurses-dev -y
 sudo apt-get install gcc-arm-linux-gnueabi -y
+sudo apt-get install gcc-arm-linux-gnueabihf -y
+sudo apt-get install libncurses-dev -y
 sudo apt-get install pv -y
 
 script_dir=`cd $(dirname $0); pwd`
@@ -10,6 +11,25 @@ KERNEL=kernel7
 
 cd ../../
 
+while true; do
+	echo -n "***WARNING***WARNING***WARNING***WARNING***WARNING***WARNING***"
+	echo -e "" 
+	lsblk
+	echo -n "***WARNING***WARNING***WARNING***WARNING***WARNING***WARNING***"
+	echo -e "" 
+	echo -n "Check your lsblk first!!! if your SD card is not on sdb stop the script now!!!"
+	echo -e ""
+	echo -n "***WARNING***WARNING***WARNING***WARNING***WARNING***WARNING***"
+	echo -e ""	
+	read -p "Do you wish to create the bootable SD card for rpi?" yn
+	case $yn in
+		[Yy]* )
+			pv -tpreb raspbian-jessie.img |sudo dd of=/dev/sdb bs=4M				
+			break;;
+		[Nn]* ) break;;
+		* ) echo "Answer yes or no.";;
+	esac
+done
 
 if [ ! -d "linux-rpi" ]; then
 
@@ -66,22 +86,38 @@ while true; do
 			
 			mkdir -p mnt/fat32
 			mkdir -p mnt/ext4
-			sleep 3s 
+			sleep 1s 
 			sudo mount /dev/sdb1 mnt/fat32
-			sleep 2s
+			sleep 1s
 			sudo mount /dev/sdb2 mnt/ext4
-			sleep 2s
+			sleep 1s
 			sudo make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- INSTALL_MOD_PATH=mnt/ext4 modules_install
-			sleep 2s
+			sleep 1s
 			sudo cp arch/arm/boot/zImage mnt/fat32/$KERNEL.img
 			sudo cp arch/arm/boot/dts/*.dtb mnt/fat32/
 			sudo cp arch/arm/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
 			sudo cp arch/arm/boot/dts/overlays/README mnt/fat32/overlays/
-			sleep 2s
+			cp -R ../vsentry/ mnt/ext4/home/pi/vsentry/
+			cd ../vsentry/vsentry-mod/files/
+			echo $PWD
+			make arm
+			echo $PWD
+			cp vsentry.ko ../../../linux-rpi/mnt/ext4/home/pi/
+			sleep 1s
+			make clean
+			cd ../../vsentry-engine/files/
+			echo $PWD
+			make arm foo=arm
+			echo $PWD
+			cp ../build/bin/sr-engine ../../../linux-rpi/mnt/ext4/home/pi/			
+			make clean
+			cd ../../../linux-rpi/
+			echo $PWD
+			sleep 1s
 			sudo umount mnt/fat32
 			sleep 2s
 			sudo umount mnt/ext4
-			sleep 2s
+			sleep 1s
 			echo -n "removing temp mnt dir..." 
 			echo -e ""
 			rm -rf mnt					
@@ -90,6 +126,7 @@ while true; do
 		* ) echo "Answer yes or no.";;
 	esac
 done
+
 
 echo -n "You need to edit the config.txt file to select the kernel that the Pi will boot into:"
 echo -e ""
