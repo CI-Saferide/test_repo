0dade9545daae84fb0ea9381bb562c27c0faa293
Shay Daniel <shayd@saferide.io>
2017-07-12 16:20:55 +0300

    [PATCH] update init_vsentry.sh add sudo to the script

--- vsentry-mod/init_vsentry.sh
+++ vsentry-mod/init_vsentry.sh
@@ -3,10 +3,10 @@ module="vsentry"
 device="vsentry"
 mode="666"
 
-rm -f /dev/${device}
+sudo rm -f /dev/${device}
 sudo rmmod $module.ko
 sudo insmod $module.ko $* || exit 1
 major=`cat /proc/devices | awk "\\$2==\"$module\" {print \\$1}"`
-mknod /dev/${device} c $major 0
-chmod $mode /dev/${device}
+sudo mknod /dev/${device} c $major 0
+sudo chmod $mode /dev/${device}
 
