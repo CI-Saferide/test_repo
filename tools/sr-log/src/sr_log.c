f54c7d96680d4146f6538466945b201d6c53c7ac
arturb-dev <beartur89@gmail.com>
2017-12-10 18:58:59 +0200

    [ADD] API for central log

--- tools/sr-log/src/sr_log.c
+++ tools/sr-log/src/sr_log.c
@@ -20,27 +20,82 @@ const static SR_8	*log_level_str[8] = {
 
 static SR_8 g_app_name[20];
 
+typedef const char* cef_str;
+FILE* log_fp = 0;
+const int max_log_size = 1024*1024*2;
+const int max_no = 4;
+cef_str cef_prefix = "cef_";
+cef_str cef_postfix = ".log";
+
+void log_cef_msg(cef_str str)
+{
+    char file1[64], file2[64];
+	int i;
+
+    if(!log_fp){
+		
+		sprintf(file1,"%s%d%s", cef_prefix, 0, cef_postfix);
+		log_fp = fopen(file1,"a");
+    }
+
+    if(log_fp){
+		if( ftell(log_fp) > max_log_size){
+			fclose(log_fp);
+            log_fp = 0;
+
+            for(i = (max_no-1);i >= 0;i--){
+				sprintf(file1,"%s%d%s",cef_prefix, i,cef_postfix );
+				sprintf(file2,"%s%d%s",cef_prefix, i+1,cef_postfix );
+				rename(file1, file2);
+			}
+
+            sprintf(file1,"%s%d%s",cef_prefix,0,cef_postfix);
+            log_fp = fopen(file1, "a");
+        }
+
+        fputs(str,log_fp);
+        fflush(log_fp);
+    }
+}
+
 void log_print_cef_msg(CEF_payload *cef)
 {
+	char cef_buffer[128];
+	char cef_class[32];
+	time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+    
+    time(&timer);
+    tm_info = localtime(&timer);
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
 
-	sal_printf("CEF: cef_version %d, vendor %s, product %s, ver %d, ",
-		cef->cef_version, cef->dev_vendor, cef->dev_product, cef->dev_version);
+	//CEF:0|SafeRide|vSentry Mobile|1.0|%d|%s|%s|%s
+	//printf("%s CEF: cef_version %d, vendor %s, product %s, ver %d, ",buffer,cef->cef_version, cef->dev_vendor, cef->dev_product, cef->dev_version);
+		
 	switch (cef->class) {
 	case NETWORK:
-		sal_printf("class network, ");
+		//sal_printf("class network, ");
+		sal_strcpy(cef_class,"class network, ");
 		break;
     case FS:
-		sal_printf("class fs, ");
+		//sal_printf("class fs, ");
+		sal_strcpy(cef_class,"class fs, ");
 		break;
     case PROC:
-		sal_printf("class proc, ");
+		//sal_printf("class proc, ");
+		sal_strcpy(cef_class,"class proc, ");
 		break;
 	default:
-		sal_printf("class N/A, ");
+		//sal_printf("class N/A, ");
+		sal_strcpy(cef_class,"class N/A, ");	
 		break;
 	}
-	sal_printf("name %s,\n", cef->name);
-	sal_printf("extension : %s\n", cef->extension);
+
+	sprintf(cef_buffer,"%s CEF: %d| vendor %s|product %s|ver %d|%s|%s|%s\n",
+			buffer,cef->cef_version, cef->dev_vendor, cef->dev_product, cef->dev_version,cef_class,cef->name, cef->extension);
+		
+	log_cef_msg(cef_buffer);
 }
 
 SR_32 sr_log_init (const SR_8* app_name, SR_32 flags)
