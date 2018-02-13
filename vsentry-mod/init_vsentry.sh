6738ce5c936135e321601acdddcff6523e9cdc4e
Shay Daniel <shayd@saferide.io>
2018-02-13 14:57:23 +0200

    minor fix to init_vsentry.sh

--- vsentry-mod/init_vsentry.sh
+++ vsentry-mod/init_vsentry.sh
@@ -3,8 +3,8 @@ module="vsentry"
 device="vsentry"
 mode="666"
 
-sudo rm -f /dev/${device}
-sudo rmmod $module.ko
+sudo rm -f /dev/${device} || true
+sudo rmmod $module.ko || true
 sudo insmod $module.ko $* || exit 1
 major=`cat /proc/devices | awk "\\$2==\"$module\" {print \\$1}"`
 sudo mknod /dev/${device} c $major 0
