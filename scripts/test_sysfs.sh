952fd5a5e2cb428dfaeb39443ed3dbcc7278f978
arturb-dev <beartur89@gmail.com>
2018-03-20 18:41:14 +0200

    [FIX] fixed the testing script according to the changes

--- scripts/test_sysfs.sh
+++ scripts/test_sysfs.sh
@@ -5,8 +5,6 @@ sleep 0.2;
 sudo sh -c "echo -n dump > /sys/kernel/vsentry/cls_file"
 sleep 0.2;
 sudo sh -c "echo -n dump > /sys/kernel/vsentry/cls_ipv4"
-sleep 0.2;
-sudo sh -c "echo -n dump > /sys/kernel/vsentry/state"
 sleep 0.5;
 
 cat /sys/kernel/vsentry/cls_can
