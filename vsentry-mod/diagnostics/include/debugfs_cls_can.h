890991e1cc85de8681a5d2260f0a89477dff5301
shay-dev <shayd@saferide.io>
2018-06-20 10:06:33 +0300

    Update debugfs_cls_can.h

--- vsentry-mod/diagnostics/include/debugfs_cls_can.h
+++ vsentry-mod/diagnostics/include/debugfs_cls_can.h
@@ -14,7 +14,7 @@ size_t dump_can_rule(SR_16 rule,char __user *user_buf, size_t count, loff_t *ppo
 
 struct debugfs_can_ent_t
 {
-	SR_8 dir[5],actionstring[16],uid_buff[16],inode_buff[16],canid_buff[16];
+	SR_8 dir[5],actionstring[32],uid_buff[32],inode_buff[16],canid_buff[16];
 	SR_U16 uid,action;
 	SR_U32 rule,inode;
 	SR_32 canid;
