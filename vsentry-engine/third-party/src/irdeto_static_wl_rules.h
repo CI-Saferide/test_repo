614d5fd0281f53ee5609103178b58bbce520546d
Your Name <you@example.com>
2019-03-10 00:21:07 -0800

    Add irdeto WL static rules.

--- vsentry-engine/third-party/src/irdeto_static_wl_rules.h
+++ vsentry-engine/third-party/src/irdeto_static_wl_rules.h
@@ -10,11 +10,21 @@ typedef struct {
 } static_file_rule_t;
 
 static static_file_rule_t irdeto_static_wl [] = {
-	/* Ruke id,  File path, permission, user,  program}, */
-/*
-	{SR_FILE_WL_START_STATIC_RULE_NO, "/work/file1.txt", "rwx", "*", "*"},
-	{SR_FILE_WL_START_STATIC_RULE_NO + 1, "/work/file2.txt", "rwx", "*", "/bin/cat"},
-*/
+	/* Rule id,  File path, permission, user,  program}, */
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/cmb_drvc"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/cmb_main"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/dpa"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/init"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/isrt"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/ivs"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/qa_client"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/qa_serfver"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/sswa_agent"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/telemetry"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/ugkp"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/unionfs"},
+	{SR_FILE_WL_START_STATIC_RULE_NO, "/customer_persistent/YAS", "rw", "*", "/usr/bin/vproxy"},
+	{SR_FILE_WL_START_STATIC_RULE_NO + 1, "/oldroot/pivot/underlay", "rwx", "*", "/usr/bin/unionfs"},
 	{0, ""},  // Must be the last entry.
 };
 
